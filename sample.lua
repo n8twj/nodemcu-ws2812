716771cac26e60ff6b536e9f85109d6589502644
n8twj <mcnamara.jeremy@gmail.com>
2015-12-16 23:31:41 -0500

    add [untested] sample

---
+++ sample.lua
@@ -0,0 +1,13 @@
+led = require('ws2812obj')
+
+b = led.rgbBar:new();
+b:set( 1, 255,   0,   0 ); -- red
+b:set( 2, 255, 128,   0 ); -- orange
+b:set( 3, 128, 255,   0 ); -- yellow
+b:set( 4,   0, 255,   0 ); -- green
+b:set( 5,   0, 255, 255 ); -- cyan
+b:set( 6,   0,   0, 255 ); -- blue
+b:set( 7, 128,   0, 255 ); -- indigo
+b:set( 8, 255,   0, 128 ); -- violet
+b:update();
+
